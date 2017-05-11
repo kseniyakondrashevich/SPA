767c3438c4ffbfb627cdbb752beb45639279814f
Kseniya <griffi.by@mail.ru>
2017-05-11 12:42:39 +0300

    added changes to main

--- main.js
+++ main.js
@@ -3,15 +3,33 @@
  */
 
 const express = require('express');
-const requestHandlers = require('./routes/mainRoute');
+const handle = require('./mainRoute');
+const bodyParser = require('body-parser');
+const database = require('./db-init');
 
 const app = express();
-app.use(express.static(__dirname + '/public'));
-//app.use(express.bodyDecoder());
-app.set('views engine', 'ejs');
 
-app.listen(8080, function () {
-    console.log('Express server listening on port 8080');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.set('views engine', 'html');
+
+
+database.get(function (err) {
+    if(err){
+        console.log('Unable connection to MySQL. ');
+        process.exit(1);
+    }
+    else{
+        app.listen(8080, function () {
+            console.log('Express server listening on port 8080');
+        });
+        app.use(handle);
+    }
 });
 
-app.use(requestHandlers);
+
+
+
+
+
